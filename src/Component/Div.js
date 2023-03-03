ac4f9655582c599033f25f6fccf734f53d2da541
Vignesh <116133745+Vignesh296169@users.noreply.github.com>
2023-03-03 14:19:50 +0530

    save

--- src/Component/Div.js
+++ src/Component/Div.js
@@ -5,7 +5,7 @@ import React from 'react'
 function Div(props) {
  
   return (<div>
-    <div>{props.title}: in Degree </div>
+    <div>{props.title}{props.title ? ":in Degree" :""}</div>
      {/* <span>{initial}</span> */}
     </div>
   )
